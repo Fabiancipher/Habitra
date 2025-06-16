8be30afce61d1db7967333454d45917d70920834
Juankusunokii <juanavila311026@gmailcom>
2025-06-15 20:27:47 -0500

    Add files via upload

--- Code V2/AddB.java
+++ Code V2/AddB.java
@@ -13,12 +13,20 @@ public class AddB extends DataB {
     public String Process(String receivedData) {
         return "Added " + this.name + " and info: " + receivedData;
     }
-
-    public void GetInfo() {
-        
+    /**
+     * This method prompts the user to enter a new name for the current object via the console,
+     * reads the input, and updates the object's name using the OverWrite method.
+     * Displays a message indicating the modification process.
+     * * @return void
+     * */
+    public void GetInfo() {Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter info for " + nameText + ":");
+        String input = scanner.nextLine();
+        System.out.println(process(input));
     }
-
-    public Boolean IsBadHabit(){
-
+        
+        public String OverWrite(String newName) {
+        this.name = newName;
+        return this.name;
     }
 }
